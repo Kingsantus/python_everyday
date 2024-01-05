b87ff101701087470c3e32535eac7c97bbf796b1
kingsantus <nkingsantus@gmail.com>
2024-01-05 05:37:55 +0100

    day 1

---
+++ Day-1/python_variable.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+# to get it is to alternate use a third variable
+c = a
+a = b
+b = c
+
+print(a)
+print(b)
+print(a+b)
---
+++ Day-1/user_input.py
@@ -0,0 +1,10 @@
+# input() will get user input in console
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# Provide any name in the input
+# that value can be accessed using the input() function
+#write your code below this line
+
+name = input("What is your name? ")
+print(len(name))
