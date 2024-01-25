5e82b9bbb9568b97e68ed7e2b89f585ede7c7fbe
kingsantus <nkingsantus@gmail.com>
2024-01-25 21:32:54 +0100

    Day-13

---
+++ Day-13/main.py
@@ -0,0 +1,21 @@
+"""Debugging"""
+# Describe the problem 
+# try to understand the code 
+# def my_function():
+#     for i in range(1,21):
+#         if i == 20:
+#             print("You got it")
+# my_function()
+
+# Reproduce the bug
+# from random import randint
+# dice_imgs = ['1','2','3','4','5','6']
+# dice_num = randint(0,5)
+# print(dice_imgs[dice_num])
+
+# Play computer 
+# year = int(input("What's your year of birth? "))
+# if year > 1980 and year < 1994:
+#     print("You are a millenial.")
+# elif year >= 1994:
+#     print("You are a Genz.")
